95ba14d4b0bc36c49f19564db2933c93b399df00
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2018-10-16 22:19:21 -0400

    Initial Commit

---
+++ SDLProject/SDLProject/Window.h
@@ -0,0 +1,21 @@
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+
+
+class Window {
+private:
+	struct SDL_Window* window;
+	struct SDL_Surface* screenSurface;
+	int width, height;
+
+public:
+	Window(int width_, int height_);
+	~Window();
+	bool OnCreate();
+	void OnDestroy();
+	SDL_Window* GetSDL_Window();
+};
+
+#endif // !WINDOW_H
